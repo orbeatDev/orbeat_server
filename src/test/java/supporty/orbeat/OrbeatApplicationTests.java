d4410bc6026f90da82ef22b41bc33f120da271e4
HyungJooKim <hyungjookim12@gmail.com>
2022-07-15 15:42:56 +0900

    [:wrench: config] 프로젝트 초기 세팅
    1. gradle setting
    2. application.yml 추가
    3. docker-compose.yml 추가

---
+++ src/test/java/supporty/orbeat/OrbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package supporty.orbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

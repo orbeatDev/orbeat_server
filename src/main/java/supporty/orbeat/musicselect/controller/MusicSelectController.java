b3c5244e56c6c55ed5b7f5dbac179ae8b8ac9af2
김성수 <poopidoopi97@naver.com>
2022-08-30 15:57:28 +0900

    라운드 음악 선택 정보 저장 API

--- src/main/java/supporty/orbeat/musicselect/controller/MusicSelectController.java
+++ src/main/java/supporty/orbeat/musicselect/controller/MusicSelectController.java
@@ -8,6 +8,7 @@ import supporty.orbeat.common.BaseResponseStatus;
 import supporty.orbeat.music.entity.Music;
 import supporty.orbeat.musicselect.MusicSelectService;
 import supporty.orbeat.musicselect.dto.BattleReq;
+import supporty.orbeat.musicselect.dto.SaveBattleReq;
 
 import java.util.Arrays;
 
@@ -20,7 +21,7 @@ public class MusicSelectController {
 
     @ApiOperation(value = "배틀 라운드 API", notes = "배틀을 진행한다.")
     @ResponseBody
-    @PostMapping("/round")
+    @PostMapping("/round/get")
     public BaseResponse getBattleInfo(@RequestBody BattleReq battleReq){
         Music firstMusic = musicSelectService.getMusicInfo(battleReq,1);
         Music secondMusic = musicSelectService.getMusicInfo(battleReq,2);
@@ -28,5 +29,12 @@ public class MusicSelectController {
             return new BaseResponse(BaseResponseStatus.INTERNAL_SERVER_ERROR);
         }
         return new BaseResponse(Arrays.asList(firstMusic, secondMusic));
+    }
+
+    @ApiOperation(value = "배틀 라운드 저장 API", notes = "각 배틀 라운드 선택 정보를 저장한다")
+    @ResponseBody
+    @PostMapping("/round/save")
+    public BaseResponse saveBattleInfo(@RequestBody SaveBattleReq saveBattleReq) {
+        return musicSelectService.saveMusicSelectInfo(saveBattleReq);
     }
 }

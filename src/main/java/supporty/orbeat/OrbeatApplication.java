b98954abd70c5a83cbd891f7c9b867dbce2dc1a0
김성수 <poopidoopi97@naver.com>
2022-09-16 02:18:32 +0900

    CORS 관련 설정 추가

--- src/main/java/supporty/orbeat/OrbeatApplication.java
+++ src/main/java/supporty/orbeat/OrbeatApplication.java
@@ -2,6 +2,9 @@ package supporty.orbeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OrbeatApplication {
@@ -10,4 +13,14 @@ public class OrbeatApplication {
 		SpringApplication.run(OrbeatApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://orbeat.co.kr", "http://www.orbeat.co.kr", "https://orbeat.co.kr", "https://www.orbeat.co.kr");
+			}
+		};
+	}
 }

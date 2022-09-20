c2ac93effcea7c2eb43d0ff872e2872d8fc60d85
김성수 <poopidoopi97@naver.com>
2022-09-20 14:32:20 +0900

    Cors, filter 기능 추가

--- src/main/java/supporty/orbeat/OrbeatApplication.java
+++ src/main/java/supporty/orbeat/OrbeatApplication.java
@@ -12,15 +12,4 @@ public class OrbeatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrbeatApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://orbeat.co.kr", "http://www.orbeat.co.kr", "https://orbeat.co.kr", "https://www.orbeat.co.kr");
-			}
-		};
-	}
 }
